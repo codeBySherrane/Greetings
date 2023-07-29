cdb9d984acdb26d4cabcbd294bf73bea6a9d082c
Sherrane Watson <Sherranewatson98@gmail.com>
2023-07-29 10:45:20 -0400

    Initial Commit

---
+++ Greetings/GreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsApp.swift
+//  Greetings
+//
+//  Created by Sherrane Watson on 7/29/23.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
